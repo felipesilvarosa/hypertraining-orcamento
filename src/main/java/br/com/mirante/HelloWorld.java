dfafc5ed171ccb0895a4f7d53f58cba6ea24f14d
Felipe Rosa <felipesrosa@gmail.com>
2021-10-21 22:26:52 -0300

    Test code

--- src/main/java/br/com/mirante/HelloWorld.java
+++ src/main/java/br/com/mirante/HelloWorld.java
@@ -1,39 +1,11 @@
 package br.com.mirante;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 
 public class HelloWorld {
 
 	public static void main(String[] args) throws SQLException {
 
-		Integer maiorId = null;
-
-		try (Connection conexao = DriverManager.getConnection("jdbc:mysql://localhost/orcamento", "root", "fel3003");
-				Statement statement = conexao.createStatement()) {
-
-			ResultSet result = statement.executeQuery("SELECT MAX(ID) AS ID FROM ORCAMENTO");
-			result.next();
-			maiorId = result.getInt("ID");
-
-		} catch (SQLException exception) {
-			System.out.println("Erro ao conectar ao servidor");
-			exception.printStackTrace();
-		}
-
-		System.out.println(maiorId);
-
-	}
-
-	static void imprimirResultado(ResultSet result) throws SQLException {
-		while (result.next()) {
-			Integer id = result.getInt("ID");
-			String nome = result.getString("NOME");
-
-			System.out.println(id + " " + nome);
-		}
+		System.out.println(System.getenv("SISTEMA_ORCAMENTO_PASSWORD"));
 	}
 }
