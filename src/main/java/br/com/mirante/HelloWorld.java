9f8038f44a1a9765babf4cf8b065e5a8eac7a026
Felipe Rosa <felipesrosa@gmail.com>
2021-10-22 22:34:16 -0300

    Teste

--- src/main/java/br/com/mirante/HelloWorld.java
+++ src/main/java/br/com/mirante/HelloWorld.java
@@ -6,6 +6,18 @@ public class HelloWorld {
 
 	public static void main(String[] args) throws SQLException {
 
-		System.out.println(System.getenv("SISTEMA_ORCAMENTO_PASSWORD"));
+		/*
+		 * EntityManagerFactory fabrica =
+		 * Persistence.createEntityManagerFactory("default");
+		 * 
+		 * EntityManager entityManager = fabrica.createEntityManager();
+		 * entityManager.getTransaction().begin(); Aluno a =
+		 * entityManager.find(Aluno.class, "Novo aluno"); Aluno a19 = new Aluno();
+		 * List<Aluno> alunos =
+		 * entityManager.createQuery("SELECT a FROM Aluno a").getResultList();
+		 * entityManager.remove(a); alunos.forEach(aluno ->
+		 * System.out.println(aluno.nome)); entityManager.getTransaction().commit(); //
+		 * entityManager.persist(new Orcamento());
+		 */
 	}
 }
