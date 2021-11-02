917d55ae7c200a10415d1eef5cc49d2e4f4065d2
Felipe Rosa <felipesrosa@gmail.com>
2021-11-01 22:07:48 -0300

    Add: constructor, getters and setters

--- src/main/java/br/com/mirante/orcamento/domain/Produto.java
+++ src/main/java/br/com/mirante/orcamento/domain/Produto.java
@@ -1,14 +1,19 @@
 package br.com.mirante.orcamento.domain;
 
-public class IdentificadorProduto {
+public class Produto {
 
     private String origem;
     private String codigo;
+    private String descricao;
+    private String unidadeMedida;
+    private Float valor;
 
-    public IdentificadorProduto(String origem, String codigo) {
-        super();
+    public Produto(String origem, String codigo, String descricao, String unidadeMedida, Float valor) {
         this.origem = origem;
         this.codigo = codigo;
+        this.descricao = descricao;
+        this.unidadeMedida = unidadeMedida;
+        this.valor = valor;
     }
 
     public String getOrigem() {
@@ -17,5 +22,17 @@ public class IdentificadorProduto {
 
     public String getCodigo() {
         return codigo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public String getUnidadeMedida() {
+        return unidadeMedida;
+    }
+
+    public Float getValor() {
+        return valor;
     }
 }
